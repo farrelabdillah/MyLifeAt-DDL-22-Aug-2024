559fb6c336fc906994299a92b4025f07d0cb1ab6
farrelabdillah <farrelabdillah@users.noreply.github.com>
2024-08-22 17:30:18 +0700

    first commit

---
+++ Farrel_LearnWebApi/Core/Features/Commands/CreateTableSpecifications/CreateTableSpecificationHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Persistence.Models;
+using Persistence.Repositories;
+
+namespace Core.Features.Commands.CreateTableSpecification
+{
+    public class CreateTableSpecificationHandler : IRequestHandler<CreateTableSpecificationCommand, CreateTableSpecificationResponse>
+    {
+        private readonly ITableSpecificationRepository _tableSpecificationRepository;
+
+        public CreateTableSpecificationHandler(ITableSpecificationRepository tableSpecificationRepository)
+        {
+            _tableSpecificationRepository = tableSpecificationRepository;
+        }
+
+        public async Task<CreateTableSpecificationResponse> Handle(CreateTableSpecificationCommand command, CancellationToken cancellationToken)
+        {
+            var newTableSpecification = new TableSpecification
+            {
+                TableId = Guid.NewGuid(),
+                TableNumber = command.TableNumber,
+                ChairNumber = command.ChairNumber,
+                TablePic = command.TablePic,
+                TableType = command.TableType
+            };
+
+            await _tableSpecificationRepository.AddAsync(newTableSpecification);
+
+            return new CreateTableSpecificationResponse
+            {
+                TableId = newTableSpecification.TableId,
+                Success = true,
+                Message = "Table specification created successfully."
+            };
+        }
+    }
+}
